364a94f5c9ee5f16fc70540dd8cf9e3392af44e5
mayinmarch <marfa.astafieva@gmail.com>
2020-03-22 20:27:33 +0300

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 !== 0) {
+    return false;
+  }
+
+  let open = {};
+  let close = {};
+  bracketsConfig.forEach((el) => {
+    open[el[0]] = el[1];
+    close[el[1]] = el[0];
+  })
+
+  let stack = [];
+  str.split('').forEach((el) => {
+    if (open[el] === close[el]){
+      if(el === stack[stack.length - 1]){
+        stack.pop();
+      } else {
+        stack.push(el);
+      }
+    }
+    else if(open[el]){
+      stack.push(el);
+    }
+    else if(close[el]){
+      if(close[el] === stack[stack.length - 1]){
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  })
+  if (stack.length !== 0){
+    return false;
+  }
+  return true;
 }
